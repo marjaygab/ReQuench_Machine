39ee0163d8f617595d671415d621b98eeac052d2
Marjay Tapay <tapaymarjay@gmail.com>
2019-02-16 20:38:55 +0800

    Changed number of pulses to 5

--- python_scripts/flowsensor.py
+++ python_scripts/flowsensor.py
@@ -25,7 +25,7 @@ try:
         rate_cnt = 0
         pulses = 0
         time_start= time.time()
-        while pulses <= 5:
+        while pulses <= 4:
             gpio_cur = GPIO.input(inpt)
             if gpio_cur != 0 and gpio_cur != gpio_last:
                 pulses += 1
