28460d37574bf080a322b05021929e21975add5b
Marjay Tapay <tapaymarjay@gmail.com>
2019-02-16 23:28:49 +0800

    Updated main.py for automatic dispensing

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -92,20 +92,34 @@ def manualDispense(command):
 def automaticDispense(command,amount_requested):
         counter = 0
         GPIO.output(inpt1, 1)
+        rate_cnt = 0
+        tot_cnt = 0
+        time_zero = 0.0
+        time_start = 0.0
+        time_end = 0.0
+        gpio_last = 0
+        pulses = 0
+        constant = 1.79
+        time_zero = time.time()
+        GPIO.output(7, 1)
         while amount_requested != counter:
-                if command == 'HOT':
-                        #dispensing hot here
-                        counter = counter + 1
-                        print(counter)
-                        sys.stdout.flush()
-                else:
-                        #dispensing cold here
-                        counter = counter + 1
-                        print(counter)
-                        sys.stdout.flush()
-                
-                time.sleep(1)
-        GPIO.output(inpt1, 0)
+                rate_cnt = 0
+                pulses = 0
+                time_start= time.time()
+                while pulses <= 5:
+                        gpio_cur = GPIO.input(11)
+                        if gpio_cur != 0 and gpio_cur != gpio_last:
+                                pulses += 1
+                        gpio_last = gpio_cur
+                rate_cnt += 1
+                tot_cnt += 1
+                time_end = time.time()
+                lmin = round((rate_cnt * constant)/(time_end-time_start),2)
+                total_liters = round(tot_cnt * constant, 1)
+                print(json.dumps({'LMin':lmin,'Total':total_liters}))
+                sys.stdout.flush()
+                time.sleep(0.5)
+        GPIO.output(7, 0)
         dispenseIsDoneAutomatic(command)
 
 def dispenseIsDoneManual(mode):
