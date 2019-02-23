5bb81e228851fe20efac1c5ff7967ebc9e4e0c71
Marjay Tapay <tapaymarjay@gmail.com>
2019-02-23 17:35:48 +0800

    Testing

--- python_scripts/flowsensor.py
+++ python_scripts/flowsensor.py
@@ -3,8 +3,8 @@ import time, sys
 import json
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-pump_1 = 13
-solenoid_1 = 19
+pump_1 = 11
+solenoid_1 = 15
 flowmeter = 40
 GPIO.setup(pump_1, GPIO.OUT)
 GPIO.setup(solenoid_1, GPIO.OUT)
@@ -19,16 +19,15 @@ time_end = 0.0
 gpio_last = 0
 pulses = 0
 constant = 1.79
-ml_constant = 16.6
+
 time_zero = time.time()
-rate_cnt = 0
-counter = 0
-GPIO.output(pump_1, 0)
-GPIO.output(solenoid_1, 0)
+
+
 try:
-    time_start= time.time()
     while True:
+        # rate_cnt = 0
         # pulses = 0
+        # time_start= time.time()
         # while pulses <= 5:
         #     gpio_cur = GPIO.input(inpt)
         #     if gpio_cur != 0 and gpio_cur != gpio_last:
@@ -36,16 +35,14 @@ try:
         #     gpio_last = gpio_cur
         # rate_cnt += 1
         # tot_cnt += 1
-        time_end = time.time()
-        time_elapsed = time_end - time_start
-        if time_elapsed >= 1:
-            counter += 1
-            total_liters = round(ml_constant * counter, 1)
-            print(json.dumps({'LMin':lmin,'Total':total_liters}))
-            sys.stdout.flush()
-            time_start = time.time()
-
-        # lmin = round((rate_cnt * constant)/(time_end-time_start),2)
+        # time_end = time.time()
+        # lmin = round((rate_cnt * constant)/(time_end-time_start),1)
+        # total_liters = lmin / 60
+        # print(json.dumps({'LMin':lmin,'Total':total_liters}))
+        # sys.stdout.flush()
+        gpio_cur = GPIO.input(flowmeter)
+        print(json.dumps(gpio_cur)
+        time.sleep(1)
 except KeyboardInterrupt:
     GPIO.cleanup()
     sys.exit
