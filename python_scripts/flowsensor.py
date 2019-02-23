a960c24cd01ee1fd0b03e07ad9a73f9cd1c833c9
Marjay Tapay <tapaymarjay@gmail.com>
2019-02-23 16:56:20 +0800

    Testing

--- python_scripts/flowsensor.py
+++ python_scripts/flowsensor.py
@@ -16,27 +16,31 @@ time_end = 0.0
 gpio_last = 0
 pulses = 0
 constant = 1.79
-
+ml_constant = 16.6
 time_zero = time.time()
+rate_cnt = 0
 
 
 try:
     while True:
-        rate_cnt = 0
-        pulses = 0
+        # pulses = 0
         time_start= time.time()
-        while pulses <= 5:
-            gpio_cur = GPIO.input(inpt)
-            if gpio_cur != 0 and gpio_cur != gpio_last:
-                pulses += 1
-            gpio_last = gpio_cur
-        rate_cnt += 1
-        tot_cnt += 1
+        # while pulses <= 5:
+        #     gpio_cur = GPIO.input(inpt)
+        #     if gpio_cur != 0 and gpio_cur != gpio_last:
+        #         pulses += 1
+        #     gpio_last = gpio_cur
+        # rate_cnt += 1
+        # tot_cnt += 1
         time_end = time.time()
-        lmin = round((rate_cnt * constant)/(time_end-time_start),2)
-        total_liters = round(tot_cnt * constant, 1)
-        print(json.dumps({'LMin':lmin,'Total':total_liters}))
-        sys.stdout.flush()
+        time_elapsed = time_end - time_start
+        if time_elapsed >= 1:
+            counter += 1
+            total_liters = round(ml_constant * counter, 1)
+            print(json.dumps({'LMin':lmin,'Total':total_liters}))
+            sys.stdout.flush()
+
+        # lmin = round((rate_cnt * constant)/(time_end-time_start),2)
 except KeyboardInterrupt:
     GPIO.cleanup()
     sys.exit
