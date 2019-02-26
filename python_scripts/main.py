c73509915c71356a02115e3e7b097408f27af2ad
Marjay Tapay <tapaymarjay@gmail.com>
2019-02-26 19:25:43 +0800

    sio

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -152,9 +152,7 @@ def manualDispense(command):
                 if time_duration == 0.05:
                         total_liters = total_liters + 1
                         time_start= time.time()
-                        # sio.emit('socket-event',{"destination":"JS","content":{"Total":total_liters}})
-                        print(total_liters)
-                        sys.stdout.flush()
+                        sio.emit('socket-event',{"destination":"JS","content":{"Total":total_liters}})
                 if checkCommand() == 'Standby':
                         total_liters = 0
                         stop_dispense()
