215d3be12597ca01fca7f25d600166de05414402
Marjay Tapay <tapaymarjay@gmail.com>
2019-02-26 19:12:13 +0800

    Testing

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -1,12 +1,12 @@
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import json
 import time
 import socketio
 import sys
 sio = socketio.Client()
 sio.connect('http://localhost:3000')
-# GPIO.setwarnings(False)
-# GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
 inpt = 11
 inpt1 = 7
 pump_1 = 11
@@ -14,22 +14,22 @@ solenoid_1 = 15
 pump_2 = 13
 solenoid_2 = 19
 flowmeter = 40
-# GPIO.setup(inpt1, GPIO.OUT)
-# GPIO.setup(pump_1, GPIO.OUT)
-# GPIO.setup(solenoid_1, GPIO.OUT)
-# GPIO.setup(pump_2, GPIO.OUT)
-# GPIO.setup(solenoid_2, GPIO.OUT)
-# GPIO.setup(flowmeter, GPIO.IN)
+GPIO.setup(inpt1, GPIO.OUT)
+GPIO.setup(pump_1, GPIO.OUT)
+GPIO.setup(solenoid_1, GPIO.OUT)
+GPIO.setup(pump_2, GPIO.OUT)
+GPIO.setup(solenoid_2, GPIO.OUT)
+GPIO.setup(flowmeter, GPIO.IN)
 mode_manual = False
 mode_auto = False
 temp_hot = False
 temp_cold = False
 auto_amount = 0
 terminate_flag = False
-# GPIO.output(pump_1,1)
-# GPIO.output(solenoid_1,1)
-# GPIO.output(pump_2,1)
-# GPIO.output(solenoid_2,1)
+GPIO.output(pump_1,1)
+GPIO.output(solenoid_1,1)
+GPIO.output(pump_2,1)
+GPIO.output(solenoid_2,1)
 @sio.on('connect')
 def on_connect():
     print("I'm connected!")
@@ -138,12 +138,12 @@ def manualDispense(command):
     constant = 1.79
     time_zero = time.time()
     gpio_cur = 0
-    # if command == 'COLD':
-    #         GPIO.output(pump_1,0)
-    #         GPIO.output(solenoid_1,0)
-    # else:
-    #         GPIO.output(pump_2,0)
-    #         GPIO.output(solenoid_2,0)
+    if command == 'COLD':
+            GPIO.output(pump_1,0)
+            GPIO.output(solenoid_1,0)
+    else:
+            GPIO.output(pump_2,0)
+            GPIO.output(solenoid_2,0)
     while checkCommand() != 'Standby':
         rate_cnt = 0
         pulses = 0
@@ -181,14 +181,14 @@ def manualDispense(command):
         # sio.emit('socket-event', {"destination":'JS',"content":'Stopped Dispense'})
         # GPIO.output(7, 0)
 
-        rate_cnt += 1
-        tot_cnt += 1
-        time_end = time.time()
-        lmin = round((rate_cnt * constant)/(time_end-time_start), 2)
-        total_liters = round(tot_cnt * constant, 1)
-        sio.emit('socket-event', {"destination": 'JS',
-                                  "content": {'LMin': lmin, 'Total': total_liters}})
-        time.sleep(0.5)
+        # rate_cnt += 1
+        # tot_cnt += 1
+        # time_end = time.time()
+        # lmin = round((rate_cnt * constant)/(time_end-time_start), 2)
+        # total_liters = round(tot_cnt * constant, 1)
+        # sio.emit('socket-event', {"destination": 'JS',
+        #                           "content": {'LMin': lmin, 'Total': total_liters}})
+        # time.sleep(0.5)
 
     # GPIO.output(pump_1,1)
     # GPIO.output(solenoid_1,1)
