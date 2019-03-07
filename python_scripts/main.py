ba1935152666cd595cf55db9e506ff16fed8dba4
Marjay Tapay <tapaymarjay@gmail.com>
2019-03-07 23:26:38 +0800

    Testing

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -49,135 +49,135 @@ terminate_flag = False
 # GPIO.output(heater,0)
 @sio.on('connect')
 def on_connect():
-    print("I'm connected!")
-    sys.stdout.flush()
+	print("I'm connected!")
+	sys.stdout.flush()
 
 
 @sio.on('socket-event')
 def on_message(data):
-    global mode_manual
-    global mode_auto
-    global temp_hot
-    global temp_cold
-    global auto_amount
-    global terminate_flag
-    global current_baseline
-    destination = data['destination']
-    if destination == 'Python':
-        command = data['content']['command']
-        if command == 'Toggle_Auto':
-            mode_auto = True
-            mode_manual = False
-        elif command == 'Toggle_Manual':
-            mode_auto = False
-            mode_manual = True
-        elif command == 'Toggle_Hot':
-            temp_cold = False
-            temp_hot = True
-        elif command == 'Toggle_Cold':
-            temp_cold = True
-            temp_hot = False
-        elif command == 'Set_Amount':
-            auto_amount = data['amount']
-        elif command == 'Stop_Dispense':
-            temp_cold = False
-            temp_hot = False
-        elif command == 'Compressor On':
-            cooling = True
-            # GPIO.output(compressor,0)
-        elif command == 'Compressor Off':
-            cooling = False
-            # GPIO.output(compressor,1)
-        elif command == 'Heater On':
-            heating = True
-            # GPIO.output(heater,0)
-        elif command == 'Heater Off':
-            heating = False
-            # GPIO.output(heater,1)
-        elif command == 'Get Baseline':
-            current_baseline = getBaseline()
-        elif command == 'Get Container':
-            current_container = getContainerWeight()
-            if current_container != False:
-                sio.emit('socket-event',{"destination":"JS","content":{"type":"CONTAINER_STATUS","body":"Container Present"}})
-            else:
-                sio.emit('socket-event',{"destination":"JS","content":{"type":"CONTAINER_STATUS","body":"Container Absent"}})
-        elif command == 'Get Current':
-            current_weight = getCurrentWeight()
-        elif command == 'Terminate':
-            # GPIO.cleanup()
-            terminate_flag = True
+	global mode_manual
+	global mode_auto
+	global temp_hot
+	global temp_cold
+	global auto_amount
+	global terminate_flag
+	global current_baseline
+	destination = data['destination']
+	if destination == 'Python':
+		command = data['content']['command']
+		if command == 'Toggle_Auto':
+			mode_auto = True
+			mode_manual = False
+		elif command == 'Toggle_Manual':
+			mode_auto = False
+			mode_manual = True
+		elif command == 'Toggle_Hot':
+			temp_cold = False
+			temp_hot = True
+		elif command == 'Toggle_Cold':
+			temp_cold = True
+			temp_hot = False
+		elif command == 'Set_Amount':
+			auto_amount = data['amount']
+		elif command == 'Stop_Dispense':
+			temp_cold = False
+			temp_hot = False
+		elif command == 'Compressor On':
+			cooling = True
+			# GPIO.output(compressor,0)
+		elif command == 'Compressor Off':
+			cooling = False
+			# GPIO.output(compressor,1)
+		elif command == 'Heater On':
+			heating = True
+			# GPIO.output(heater,0)
+		elif command == 'Heater Off':
+			heating = False
+			# GPIO.output(heater,1)
+		elif command == 'Get Baseline':
+			current_baseline = getBaseline()
+		elif command == 'Get Container':
+			current_container = getContainerWeight()
+		if current_container != False:
+			sio.emit('socket-event',{"destination":"JS","content":{"type":"CONTAINER_STATUS","body":"Container Present"}})
+		else:
+			sio.emit('socket-event',{"destination":"JS","content":{"type":"CONTAINER_STATUS","body":"Container Absent"}})
+		elif command == 'Get Current':
+			current_weight = getCurrentWeight()
+		elif command == 'Terminate':
+			# GPIO.cleanup()
+			terminate_flag = True
 
 
 @sio.on('disconnect')
 def on_disconnect():
-    print("I'm disconnected!")
-    sys.stdout.flush()
+	print("I'm disconnected!")
+	sys.stdout.flush()
 
 
 def check_operation():
-    if mode_manual:
-        return 'Manual'
-    else:
-        return 'Automatic'
+	if mode_manual:
+		return 'Manual'
+	else:
+		return 'Automatic'
 
 def getBaseline():
-    val = hx.get_weight_A(5)
-    baseline = (val // 1000) * 1000
-    return baseline
+	val = hx.get_weight_A(5)
+	baseline = (val // 1000) * 1000
+	return baseline
 
 def getCurrentWeight():
-    val = hx.get_weight_A(5)
-    return val
+	val = hx.get_weight_A(5)
+	return val
 
 def getContainerWeight():
-    container_weight = ((getCurrentWeight() - baseline) / 200) - 4
+	container_weight = ((getCurrentWeight() - baseline) / 200) - 4
 	if container_weight < 0:
 		container_weight = 0
-        return False
-    else:
-        return container_weight
+		return False
+	else:
+		return container_weight
 
 def checkCommand():
-    if temp_hot and not temp_cold:
-        return 'Dispense_Hot'
-    elif temp_cold and not temp_hot:
-        return 'Dispense_Cold'
-    elif not temp_cold and not temp_hot:
-        return 'Standby'
-    else:
-        return 'Error'
+	if temp_hot and not temp_cold:
+		return 'Dispense_Hot'
+	elif temp_cold and not temp_hot:
+		return 'Dispense_Cold'
+	elif not temp_cold and not temp_hot:
+		return 'Standby'
+	else:
+		return 'Error'
 
 
 def getRequestedAmount():
-    return auto_amount
+    	return auto_amount
 
 
 def manualMode(command):
-    if(command == 'Dispense_Hot'):
-        # dispense hot here
-        manualDispense('HOT')
-    elif(command == 'Dispense_Cold'):
-        # dispense cold here
-        manualDispense('COLD')
+	if(command == 'Dispense_Hot'):
+		# dispense hot here
+		manualDispense('HOT')
+	elif(command == 'Dispense_Cold'):
+		# dispense cold here
+		manualDispense('COLD')
 
 
 def automaticMode(command):
-    amount_requested = auto_amount
-    if amount_requested != 0:
-        if(command == 'Dispense_Hot'):
-            # dispense hot here
-            automaticDispense('HOT', amount_requested)
-        elif(command == 'Dispense_Cold'):
-            # dispense cold here
-            automaticDispense('COLD', amount_requested)
+	amount_requested = auto_amount
+	if amount_requested != 0:
+		if(command == 'Dispense_Hot'):
+		# dispense hot here
+		automaticDispense('HOT', amount_requested)
+		elif(command == 'Dispense_Cold'):
+		# dispense cold here
+		automaticDispense('COLD', amount_requested)
 
 
 def stop_dispense():
-    global temp_hot
-    global temp_cold
-    temp_cold = False
-    temp_hot = False
+	global temp_hot
+	global temp_cold
+	temp_cold = False
+	temp_hot = False
 
 
 def manualDispense(command):
@@ -187,18 +187,18 @@ def manualDispense(command):
         # else:
         #         GPIO.output(pump_2,0)
         #         GPIO.output(solenoid_2,0)        
-        time_start = time.time()
-        while checkCommand() != 'Standby':
-            if time_duration == 1:
-                    total_liters = getCurrentWeight() - current_container;
-                    if total_liters < 0:
-                        total_liters = 0
-                    time_start= time.time()
-                    sio.emit('socket-event',{"destination":"JS","content":{"type":"DISPENSE_READING","body":{"Total":total_liters}}})
-            if checkCommand() == 'Standby':
-                    total_liters = 0
-                    stop_dispense()
-                    break
+	time_start = time.time()
+	while checkCommand() != 'Standby':
+		if time_duration == 1:
+			total_liters = getCurrentWeight() - current_container;
+			if total_liters < 0:
+				total_liters = 0
+			time_start= time.time()
+			sio.emit('socket-event',{"destination":"JS","content":{"type":"DISPENSE_READING","body":{"Total":total_liters}}})
+		if checkCommand() == 'Standby':
+			total_liters = 0
+			stop_dispense()
+			break
         # GPIO.output(pump_1,1)
         # GPIO.output(solenoid_1,1)
         # GPIO.output(pump_2,1)
@@ -206,12 +206,12 @@ def manualDispense(command):
 
 
 def automaticDispense(command, amount_requested):
-    counter = 0
-    while counter < amount_requested:
-        counter = counter + 1
-        time.sleep(0.5)
-    sio.emit('socket-event', {"destination": 'JS',
-                              "content": {"type":"DISPENSE_CONTROL","body":'Stopped Dispense'}})
+	counter = 0
+	while counter < amount_requested:
+		counter = counter + 1
+		time.sleep(0.5)
+	sio.emit('socket-event', {"destination": 'JS',
+				"content": {"type":"DISPENSE_CONTROL","body":'Stopped Dispense'}})
     stop_dispense()
     # rate_cnt = 0
     # tot_cnt = 0
@@ -247,79 +247,79 @@ def automaticDispense(command, amount_requested):
 
 
 def readTemp():
-    global sio     
-    global cold_probe_path
-    global hot_probe_path
-    global compressor
-    global heater
-    while True:
-        f = open(cold_probe_path,'r')
-        cold_temp = ""
-        lines = f.readlines()
-        f.close()
-        equal_pos = lines[1].find('t=')
-        if equal_pos != 1:
-            cold_temp = float(lines[1][equal_pos+2:])/1000.0
-        f = open(hot_probe_path,'r')
-        hot_temp = ""
-        lines = f.readlines()
-        f.close()
-        equal_pos = lines[1].find('t=')
-        if equal_pos != 1:
-            hot_temp = float(lines[1][equal_pos+2:])/1000.0
-        
-        if cold_temp <= 6:
-            cooling = False
-            # GPIO.output(compressor,1)
-        elif cold_temp >= 11:
-            cooling = True
-            # GPIO.output(compressor,0)
-        if hot_temp <= 55:
-            heating = True
-            # GPIO.output(heater,0)
-        elif hot_temp >= 70:
-            heating = False
-            # GPIO.output(heater,1)
-        sio.emit('socket-event',{"destination":"JS","content":{"type":"TEMP_READING","body":{"Cold":cold_temp,"Hot":hot_temp}}})
-        time.sleep(1)
+	global sio     
+	global cold_probe_path
+	global hot_probe_path
+	global compressor
+	global heater
+	while True:
+		f = open(cold_probe_path,'r')
+		cold_temp = ""
+		lines = f.readlines()
+		f.close()
+		equal_pos = lines[1].find('t=')
+		if equal_pos != 1:
+		cold_temp = float(lines[1][equal_pos+2:])/1000.0
+		f = open(hot_probe_path,'r')
+		hot_temp = ""
+		lines = f.readlines()
+		f.close()
+		equal_pos = lines[1].find('t=')
+		if equal_pos != 1:
+		hot_temp = float(lines[1][equal_pos+2:])/1000.0
+		
+		if cold_temp <= 6:
+		cooling = False
+		# GPIO.output(compressor,1)
+		elif cold_temp >= 11:
+		cooling = True
+		# GPIO.output(compressor,0)
+		if hot_temp <= 55:
+		heating = True
+		# GPIO.output(heater,0)
+		elif hot_temp >= 70:
+		heating = False
+		# GPIO.output(heater,1)
+		sio.emit('socket-event',{"destination":"JS","content":{"type":"TEMP_READING","body":{"Cold":cold_temp,"Hot":hot_temp}}})
+		time.sleep(1)
 
 def controller():
-    global mode_manual
-    global mode_auto
-    global temp_hot
-    global temp_cold
-    global auto_amount
-    global terminate_flag
-    while True:
-        mode = check_operation()
-        if (mode == 'Manual'):
-            command = checkCommand()
-            manualMode(command)
-        else:
-            command = checkCommand()
-            automaticMode(command)
-        if terminate_flag:
-            break
-        time.sleep(1)
+    	global mode_manual
+	global mode_auto
+	global temp_hot
+	global temp_cold
+	global auto_amount
+	global terminate_flag
+	while True:
+		mode = check_operation()
+		if (mode == 'Manual'):
+		command = checkCommand()
+		manualMode(command)
+		else:
+		command = checkCommand()
+		automaticMode(command)
+		if terminate_flag:
+		break
+		time.sleep(1)
 
 
 
 def main():
-    print('Ready')
-    sys.stdout.flush()
+    	print('Ready')
+    	sys.stdout.flush()
 
-    threading.Thread(target=controller).start()
-    threading.Thread(target=readTemp).start()
+    	threading.Thread(target=controller).start()
+    	threading.Thread(target=readTemp).start()
 
-    while True:
-        pass
+	while True:
+		pass
 try:
-    main()
+    	main()
 except KeyboardInterrupt:
-    pass
+    	pass
 except Exception as exception:
-    print(exception)
+    	print(exception)
 finally:
-    # GPIO.cleanup()
-    sio.disconnect()
-    sys.exit()
+    	# GPIO.cleanup()
+    	sio.disconnect()
+    	sys.exit()
