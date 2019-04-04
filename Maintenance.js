c8fc8ff1da8373446708a76594514a92c1b55055
Marjay Tapay <tapaymarjay@gmail.com>
2019-04-05 01:55:59 +0800

    Testing

--- Maintenance.js
+++ Maintenance.js
@@ -1,10 +1,12 @@
 let $ = require('jquery');
 let fs = require('fs');
 const io = require('socket.io-client');
+let moment = require('moment');
 var socket = io('http://localhost:3000');
 const path = require('path');
 let Swal = require('sweetalert2');
-
+let maintenance_data = require('./maintenance_data');
+let machine_settings = require('./machine_settings');
 const draining_time = 5;
 $(document).ready(function() {
     var main_container_div = document.getElementById('main_container_div');
@@ -18,12 +20,38 @@ $(document).ready(function() {
     var toggle_timer = document.getElementById('toggle_timer');
     var reset_timer = document.getElementById('reset_timer');
     var steps_control = document.getElementsByClassName('steps_control');
+    var start_drain = document.getElementById('start_drain');
+    var stop_drain = document.getElementById('stop_drain');
     var steps_size;
     var steps_list;
     var timer;
     var minute;
     var seconds;
     var current_step_selected;
+    var maintenance_data_file = maintenance_data;
+    var settings = machine_settings;
+
+    start_drain.onclick = function() {
+        this.disabled = true;
+        stop_drain.disabled = false;
+        
+        left_button.disabled = true;
+        right_button.disabled = true;
+        commandPy(socket, { command: 'Start_Drain' });
+    }
+
+    stop_drain.onclick = function() {
+        this.disabled = true;
+        start_drain.disabled = false;
+        if (!maintenance_data.from_maintenance) {
+            left_button.disabled = false;
+            right_button.disabled = false;
+        }else{
+            right_button.disabled = false;
+        }
+        
+        commandPy(socket, { command: 'Stop_Drain' });
+    }
 
     readStepsFile(function(list,size) {
         steps_list = list;
@@ -104,13 +132,19 @@ $(document).ready(function() {
                         right_button.innerHTML = 'Finish';
                     }   
                 }else{
-                    commandPy(socket, { command: 'Terminate' });
+                    
                     maintenance_data_file.from_maintenance = false;
                     maintenance_data_file.current_step = 0;
-                    window.location.assign('login.html');
+                    settings.last_maintenance_date = moment().format('YYYY-MM-DD');
                     fs.writeFile('./maintenance_data.json', JSON.stringify(maintenance_data_file,null,6), function (err) {
                         if (err) return console.log(err);
+                        fs.writeFile('./machine_settings.json', JSON.stringify(settings,null,6), function (err) {
+                            if (err) return console.log(err);
+                            window.location.assign('login.html');        
+                        }); 
                     });
+                    
+
                 }
             }
 
@@ -127,27 +161,51 @@ $(document).ready(function() {
             console.log(current_step_selected);
             
             if (current_step_selected == 1 || current_step_selected == 5 || current_step_selected == 7) {
+                document.getElementById('drain_controls').style.visibility = 'visible';
+
                 //run python here
-                commandPy(socket, { command: 'Start_Drain' });
-                var counter = 0;
-                left_button.disabled = true;
-                right_button.disabled = true;
-                var drain_interval = setInterval(() => {
-                    counter++;
-                    console.log('Draining');
-                    if (counter == draining_time) {
-                        //command python here
-                        if (maintenance_data_file.from_maintenance) {
-                            left_button.disabled = true;
-                        }else{
-                            left_button.disabled = false;
-                        }
-                        right_button.disabled = false;
-                        console.log('Draining Done');
-                        commandPy(socket, { command: 'Stop_Drain' });
-                        clearInterval(drain_interval);
+                // commandPy(socket, { command: 'Start_Drain' });
+                // var counter = 0;
+                // left_button.disabled = true;
+                // right_button.disabled = true;
+                // var drain_interval = setInterval(() => {
+                //     counter++;
+                //     console.log('Draining');
+                //     if (counter == draining_time) {
+                //         //command python here
+                //         if (maintenance_data_file.from_maintenance) {
+                //             left_button.disabled = true;
+                //         }else{
+                //             left_button.disabled = false;
+                //         }
+                //         right_button.disabled = false;
+                //         console.log('Draining Done');
+                //         commandPy(socket, { command: 'Stop_Drain' });
+                //         clearInterval(drain_interval);
+                //     }
+                // }, 60000);
+            }else if(current_step_selected == '11'){
+                document.getElementById('drain_controls').style.visibility = 'hidden';
+                Swal.fire({
+                    title: 'Are you done?',
+                    text: "You may skip refilling if need.",
+                    type: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes',
+                    cancelButtonText: "Skip Refill",
+                    allowOutsideClick: false
+                  }).then((result) => {
+                      console.log(result);
+                    if (result.dismiss == 'cancel') {
+                        settings.current_water_level = 0;
+                    }else{
+                        settings.current_water_level = 20000;                        
                     }
-                }, 60000);
+                  })
+            }else{
+                document.getElementById('drain_controls').style.visibility = 'hidden';
             }
 
             
